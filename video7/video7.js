e68c5d04a8a47de25563ce2cbb30d410c267acb0
Thanh Hao <haon8563@gmail.com>
2024-07-20 10:54:22 +0700

    chapter 2, video 8

--- video7/video7.js
+++
@@ -1,6 +0,0 @@
-console.log(`Hello world from HTML`);
-
-let a = 5;
-let b = 10;
-console.log(`Check string a = ${a} , b = ${b} , a + b = ${a + b}`);
-console.log(object);
---
+++ video8/video8.js
@@ -0,0 +1,13 @@
+console.log(`Hello world from HTML`);
+
+//key: value
+let obj = {
+    name: 'Hao',
+    address: 'TPHCM'
+};
+
+let b = 'Hao';
+
+console.log(`what is your name? , My name's ${obj["name"]}`);
+console.log(`where are you from? , i'm from ${obj.address}`);
+
